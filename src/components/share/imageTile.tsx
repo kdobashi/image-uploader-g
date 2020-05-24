40efe68601c5779de804bf770709bfdc6fd2b67e
土橋和也 <k.d.programing@gmail.com>
2020-05-24 18:22:25 +0900

    正方形化失敗した部分を元に戻す

--- src/components/share/imageTile.tsx
+++ src/components/share/imageTile.tsx
@@ -7,26 +7,7 @@ type Props = {
     imageBinary: string,
 }
 
-// スタイル
-const useStyles = makeStyles(() => ({
-    imageBox: {
-        width:"100%",
-        height:"auto",
-        paddingTop:"100%",
-        position:"relative",
-    },
-    imageSpan: {
-        position:"absolute",
-        top:"0",
-        right:"0",
-        bottom:"0",
-        left:"0",
-    }
-    
-  }));
-
 const ImageTile: React.FC<Props> = (props: Props) => {
-    const classes = useStyles();
     const [isOpen, setIsOpen] = useState<boolean>(false);
     
     const onClickImage = () => {
@@ -39,14 +20,12 @@ const ImageTile: React.FC<Props> = (props: Props) => {
     return (
         <>
             <ImageDialog isOpen={isOpen} imageBinary={props.imageBinary} onClose={onClose} />
-            <Box className={classes.imageBox}><span className={classes.imageSpan}>
-                <CardMedia
-                    key={props.id}
-                    component="img"
-                    image={props.imageBinary}
-                    onClick={onClickImage}
-                />
-            </span></Box>
+            <CardMedia
+                key={props.id}
+                component="img"
+                image={props.imageBinary}
+                onClick={onClickImage}
+            />
         </>
     )
 }
